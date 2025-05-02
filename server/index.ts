9a3d567db06328b4bcec7e837547f52bba4716cd
MannPATIRA <mannpatira@hotmail.com>
2025-05-02 09:30:00 +0100

    fix: dotenv working

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -69,12 +70,15 @@ app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  const port = 5001;
+  // server.listen({
+  //   port,
+  //   host: "localhost",
+  //   reusePort: true,
+  // }, () => {
+  //   log(`serving on port ${port}`);
+  // });
+  server.listen(port, () => {
     log(`serving on port ${port}`);
   });
 })();
